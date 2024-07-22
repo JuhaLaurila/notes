e99607cb1cd5b64e2371d2d23866cf5e3dd68905
JuhaLaurila <95275811+JuhaLaurila@users.noreply.github.com>
2024-07-22 16:36:54 +0300

    frontmukana2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.use(express.static('dist'))
+
 
 app.use(express.json())
 
@@ -8,6 +8,7 @@ app.use(express.json())
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('dist'))
 
 
 let notes = [
